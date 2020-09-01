9344d01598464448e5102ded5011ff99d3aa09db
Henry Cook <henrycook@Henrys-Air.lan>
2020-08-31 21:26:06 -0400

    Fixed responsiveness projects

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dc23d169b46d19f74858ab271498064.js"
+  "/precache-manifest.7b88df896d04282532976cfed3bf5429.js"
 );
 
 self.addEventListener('message', (event) => {
